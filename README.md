a5063c72e4028c5d2f11cdbc43479018dc5f6050
marqdouj <16510416+marqdouj@users.noreply.github.com>
2025-03-13 08:31:59 -0600

    clone/update solution

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Geolocation
+# Geolocation Service
+- RCL wrapper to the Html Geolocation API
+
+## NOTES: 
+### This was a clone of 'BlazorDeviceInterop' [here](https://github.com/darnton/BlazorDeviceInterop).
+### The GitHub project targets .NET 5.0 and has not been updated since Nov 14, 2021 so I cloned it and updated to .NET 8.0
---
+++ src/Marqdouj.Html.Geolocation/Marqdouj.Html.Geolocation/GeolocationService.cs
@@ -0,0 +1,60 @@
+﻿using Marqdouj.Html.Geolocation.Models;
+using Microsoft.JSInterop;
+
+namespace Marqdouj.Html.Geolocation
+{
+    /// <summary>
+    /// An implementation of <see cref="IGeolocationService"/> that provides 
+    /// an interop layer for the device's Geolocation API.
+    /// </summary>
+    public class GeolocationService(IJSRuntime jsRuntime) : IGeolocationService, IAsyncDisposable
+    {
+        private readonly Lazy<Task<IJSObjectReference>> moduleTask = new(() => 
+            jsRuntime.InvokeAsync<IJSObjectReference>(
+                "import", "./_content/Marqdouj.Html.Geolocation/js/Geolocation.js").AsTask());
+
+        public event EventHandler<GeolocationEventArgs> WatchPositionReceived;
+
+        /// <inheritdoc />
+        public async ValueTask<GeolocationResult> GetCurrentPosition(PositionOptions options = null)
+        {
+            var module = await moduleTask.Value;
+            return await module.InvokeAsync<GeolocationResult>("Geolocation.getCurrentPosition", options);
+        }
+
+        /// <inheritdoc />
+        public async ValueTask<long?> WatchPosition(PositionOptions options = null)
+        {
+            var module = await moduleTask.Value;
+            var callbackObj = DotNetObjectReference.Create(this);
+            return await module.InvokeAsync<int>("Geolocation.watchPosition",
+                callbackObj, nameof(SetWatchPosition), options);
+        }
+
+        /// <inheritdoc />
+        [JSInvokable]
+        public void SetWatchPosition(GeolocationResult watchResult)
+        {
+            WatchPositionReceived?.Invoke(this, new GeolocationEventArgs
+            {
+                GeolocationResult = watchResult
+            });
+        }
+
+        /// <inheritdoc />
+        public async ValueTask ClearWatch(long watchId)
+        {
+            var module = await moduleTask.Value;
+            await module.InvokeVoidAsync("Geolocation.clearWatch", watchId);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (moduleTask.IsValueCreated)
+            {
+                var module = await moduleTask.Value;
+                await module.DisposeAsync();
+            }
+        }
+    }
+}
